663da5f10aab95bbeb52d54ed0af8466c4c8496e
João Jardim <joaopnj19@gmail.com>
2021-07-25 19:09:44 -0300

    Removing warnings

--- middleware/mongoMiddleware.js
+++ middleware/mongoMiddleware.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 
 var MongoMiddleware = {
     connect: () => {
-        mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true }, (err) => {
-            if(err) console.error('Erro ao conectar no mongodb '+ err);
+        mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        }, (err) => {
+            if (err) console.error('Erro ao conectar no mongodb ' + err);
         });
     },
     disconnect: () => {
